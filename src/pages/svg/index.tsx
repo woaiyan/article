b8f60af8c4774e1b39b1956816a746ac0c6bb775
woaiyan <2422400672@qq.com>
2022-05-11 22:54:35 +0800

    111

--- src/pages/svg/index.tsx
+++ src/pages/svg/index.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-
-const Svg = () => {
-    return <div></div>
+import React, {useEffect, useState} from "react";
+import './index.less';
+const Svg: React.FC = () => {
+    const datas = [1, 2, 3, 4, 1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,];
+    return (<div className='svg-global' id='svg-global'>
+        {datas.map((e:any, index: number) => <div key={String(index)} className="svg-item">
+            {e}
+        </div>)}
+    </div>)
 }
 export default Svg;
